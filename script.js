798aa2013cdb7379102e2cf755bc631959c515f7
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-25 17:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
+    function updateTimer() {
+      var now = new Date();
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+      var day = now.getDate();
+      var month = now.getMonth() + 1; 
+      var year = now.getFullYear();
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+      var hours = now.getHours();
+      var minutes = now.getMinutes();
+      var seconds = now.getSeconds();
+
+      // Format single-digit values with leading zeros
+      if (day < 10) day = '0' + day;
+      if (month < 10) month = '0' + month;
+      if (hours < 10) hours = '0' + hours;
+      if (minutes < 10) minutes = '0' + minutes;
+      if (seconds < 10) seconds = '0' + seconds;
+
+      var timerText = day + '/' + month + '/' + year + ', ' + hours + ':' + minutes + ':' + seconds;
+      document.getElementById('timer').innerHTML = timerText;
+    }
+
+    // Update the timer every second (1000 milliseconds)
+    setInterval(updateTimer, 1000);
